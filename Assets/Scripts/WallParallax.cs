e0f5aa458cd5277b61bbc02670783085bcdc2e67
Arseniy Varganov <varganovarsen@gmail.com>
2024-05-09 12:18:24 +0300

    Dynamic filler scale

--- Assets/Scripts/WallParallax.cs
+++ Assets/Scripts/WallParallax.cs
@@ -12,13 +12,23 @@ public class WallParallax : MonoBehaviour
     [SerializeField]
     Transform _filler;
     SpriteRenderer _fillerSpr;
+    [SerializeField]
+    Transform _ender;
+
+    [SerializeField]
+    SpriteMask _tunnelMask;
     private void OnValidate()
     {
         _fillerSpr = _filler.GetChild(0).GetComponent<SpriteRenderer>();
+        SpriteRenderer _enderSpr = _ender.GetChild(0).GetComponent<SpriteRenderer>();
+        _ender.position = new Vector3(_ender.position.x, _filler.position.y - _fillerSpr.size.y);
+        endPosition = new Vector3(_ender.position.x, Mathf.Abs(_ender.position.y - 20f));
+        Transform pivot = _tunnelMask.transform.parent;
+        pivot.localScale = new Vector3(pivot.localScale.x, _enderSpr.size.y * 2 + _fillerSpr.size.y);
     }
 
 
-    [SerializeField, Range(0,0.1f)]
+    [SerializeField, Range(0, 0.1f)]
     float scrollSpeedMultiplier;
     private void Start()
     {
@@ -39,7 +49,7 @@ public class WallParallax : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Vector3 size = new Vector3(_fillerSpr.size.x * 2 + 2, _fillerSpr.size.y);
-        Gizmos.DrawWireCube(_filler.position, size);
+        Vector3 size = _fillerSpr.size;
+        Gizmos.DrawWireCube(new Vector3(_filler.position.x, _filler.position.y - size.y / 2), size);
     }
 }
